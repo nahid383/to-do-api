19477574df12bea638488118f1a7d461d18a089d
Md. Nahid Hasan <nahid1719abd@gmail.com>
2026-06-29 19:23:57 +0600

    database connection

--- server.js
+++ server.js
@@ -1,21 +1,26 @@
 import express from 'express';
 import cors from 'cors';
+import dotenv from 'dotenv';
 import todoRoutes from './routes/todo.routes.js';
+import { connectDB } from './config/db.js';
+
+dotenv.config();
 
 const app = express();
-const PORT = 3001
 
-//Middleware
+const PORT = process.env.PORT || 3000;
+
+// Middleware
 app.use(cors());
 app.use(express.json());
 
-//Test Route
+//connect to DB
+connectDB();
+
+// Routes
 app.use('/', todoRoutes);
 
-//Start Server
+// Start Server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
-
-
-
