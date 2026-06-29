27f26e260382329484bfdd258e8c1dc569dfe7f4
Md. Nahid Hasan <nahid1719abd@gmail.com>
2026-06-29 19:25:11 +0600

    commit

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ connectDB();
 app.use('/', todoRoutes);
 
 // Start Server
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
